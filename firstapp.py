b516de42f877c9739880c6438a6c707af365d69c
JohanFED21 <johansfed21@gmail.com>
2023-08-29 10:16:04 +0200

    First commit into skool

---
+++ firstapp.py
@@ -0,0 +1,14 @@
+greeting = "Hello and welcome"
+integer = 10
+smool = 0.14
+boolean = True
+thelist = ["Hello", "Wello"]
+woop = {"Hello": 5, "Potato": 25}
+print(greeting)
+
+print(type(greeting))
+print(type(integer))
+print(type(smool))
+print(type(boolean))
+print(type(thelist))
+print(type(woop))
